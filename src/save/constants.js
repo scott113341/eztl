a2bd128dd3b5fcadde1abafd2f6ea107d22a647a
Scott Hardy <scott.the.hardy@gmail.com>
2016-03-26 12:17:42 -0700

    Removed old files

--- src/save/constants.js
+++
@@ -1,5 +0,0 @@
-export const STRING_TYPE = 'String';
-export const STRING_VARIABLE_TYPE = 'StringVariable';
-export const IF_CONTROL_TYPE = 'IfControl';
-export const ELSE_CONTROL_TYPE = 'ElseControl';
-export const END_CONTROL_TYPE = 'EndControl';
